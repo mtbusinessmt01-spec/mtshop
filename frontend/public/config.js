52ab2965a2b205c0e52ed72574b44ff5e9b6030b
MTshop <you@example.com>
2026-09-21 13:26:45 +0500

    Backend manzilini yangilash

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,4 +1,1 @@
-// Backend server manzili.
-// Mahalliy (noutbukda) sinash uchun: http://localhost:4000
-// Render'ga joylashtirilgandan keyin: https://sizning-nomingiz.onrender.com (oxirida / bo'lmasin)
-const API_BASE = 'http://localhost:4000';
+const API_BASE = 'https://mtshop-p6gp.onrender.com';
